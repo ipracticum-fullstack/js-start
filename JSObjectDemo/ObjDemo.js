d78734439468d9432734ef0abadba8f13c4e7b37
Vineet <vineetk2040@gmail.com>
2023-07-18 14:53:21 +0530

    Added page display methods

--- JSObjectDemo/ObjDemo.js
+++ JSObjectDemo/ObjDemo.js
@@ -7,7 +7,6 @@ There are four different ways to create objects in Java Script:
 4. Object.create Method.
 */
 
-<<<<<<< HEAD
 //1. Object Literals.
 const fullStackProgrammer = {
   name: "Sagar",
@@ -18,17 +17,6 @@ const fullStackProgrammer = {
 };
 
 //2. Constructor Functions.
-=======
-const fullStackProgrammer = {
-  name: "Sagar",
-  mobile: "983483472831",
-  email: "ss@sagar.com",
-  expertise: "Java Script, CSS, react",
-};
-
-displayObjectInConsole(fullStackProgrammer);
-
->>>>>>> 1fa6d9449da813059a02972543155497eb5451dc
 function createFullStackProgrammer(name, mobile, email, expertise) {
   this.name = name;
   this.mobile = mobile;
@@ -49,7 +37,6 @@ const fullStackProgrammer2 = new createFullStackProgrammer(
   "Node.js"
 );
 
-<<<<<<< HEAD
 //4. Object.create Method.
 const fullStackProgrammer3 = Object.create(fullStackProgrammer2);
 fullStackProgrammer3.name = "Sujeet";
@@ -60,10 +47,6 @@ fullStackProgrammer3.email = "Sujeet@Sujeet.com";
  ********************Followings are a number of useful display methods ******************
  ****************************************************************************************
  */
-=======
-displayObjectInConsole(fullStackProgrammer1);
-displayObjectInConsole(fullStackProgrammer2);
->>>>>>> 1fa6d9449da813059a02972543155497eb5451dc
 
 function displayObjectInConsole(fsProgrammer) {
   console.log("");
@@ -73,7 +56,6 @@ function displayObjectInConsole(fsProgrammer) {
   console.log(fsProgrammer["expertise"]);
   console.log("");
 }
-<<<<<<< HEAD
 
 function displayObjectInPage(fsProgrammer) {
   var htmlElement = document.getElementById("root");
@@ -113,5 +95,3 @@ displayObjectInConsole(fullStackProgrammer);
 displayObjectInPage(fullStackProgrammer1);
 displayObjectInPageUsingLoop(fullStackProgrammer2);
 displayObjectInPageUsingLoop(fullStackProgrammer3);
-=======
->>>>>>> 1fa6d9449da813059a02972543155497eb5451dc
